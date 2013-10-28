fa6539f4d7397ff4b9b05f6284f25d763eaac3bd
Djent- <inflames2361@aim.com>
2013-10-27 23:19:36 -0400

    Now with 200% more local variables!!!

--- heapsort.lua
+++ heapsort.lua
@@ -1,12 +1,11 @@
 function heapSort(a)
-	c = #a
+	local c = #a
 	heap(a,c)
-	e = c
 
-	while e > 1 do
-		swap(a,e,1)
-		e = e - 1
-		siftDown(a,1,e)
+	while c > 1 do
+		swap(a,c,1)
+		c = c - 1
+		siftDown(a,1,c)
 	end
 end
 
@@ -19,10 +18,10 @@ function heap(a,c)
 end
 
 function siftDown(a,start,en)
-	root = start
+	local root = start
 
 	while root * 2 <= en do
-		child = root * 2
+		local child = root * 2
 		if child + 1 <= en and a[child] < a[child + 1] then
 			child = child + 1
 		end
